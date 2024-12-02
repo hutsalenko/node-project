0406f9930cb116738698ded8396821f6498633cf
Ivan <ivangutsalenko92@gmail.com>
2024-12-02 18:09:10 +0200

    Add async request

--- controllers/admin.js
+++ controllers/admin.js
@@ -127,8 +127,9 @@ exports.getProducts = (req, res) => {
         });
 };
 
-exports.postDeleteProduct = (req, res) => {
-    const prodId = req.body.productId;
+exports.deleteProduct = (req, res) => {
+    const prodId = req.params.productId;
+
     Product.findById(prodId)
         .then((product) => {
             if (!product) {
@@ -139,11 +140,9 @@ exports.postDeleteProduct = (req, res) => {
         })
         .then(() => {
             console.log('DESTROYED PRODUCT');
-            res.redirect('/admin/products');
+            res.status(200).json({ message: 'Success!' });
         })
         .catch((err) => {
-            const error = new Error(err);
-            error.httpStatusCode = 500;
-            return next(error);
+            res.status(500).json({ message: 'Deleting product failed.' });
         });
 };
